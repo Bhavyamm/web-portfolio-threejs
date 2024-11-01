855e448e89d40ee917a5e419e3e8af46b631a02e
Bhavyamm <bhavyamm01@gmail.com>
2024-11-02 01:00:24 +0530

    Added footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from 'components/sections/About'
 import Contact from 'components/sections/Contact'
+import Footer from 'components/sections/Footer'
 import Hero from 'components/sections/Hero'
 import Navbar from 'components/sections/Navbar'
 import Projects from 'components/sections/Projects'
@@ -13,6 +14,7 @@ const Home = () => {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   )
 }
