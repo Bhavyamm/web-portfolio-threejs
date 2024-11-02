d9e1bf43b8e208aac4d6e01bc47fc2bbeb8223f8
Bhavyamm <bhavyamm01@gmail.com>
2024-11-02 21:12:10 +0530

    added vercel page speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/global.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: "Bhavyam's Portfolio",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
