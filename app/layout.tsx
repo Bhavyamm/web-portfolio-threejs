01d353a955521d85961a3e34065235b342dfc0df
Bhavyamm <bhavyamm01@gmail.com>
2024-11-02 21:08:15 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,16 @@ import '@/global.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
+  title: "Bhavyam's Portfolio",
+  description: 'This portfolio designed using Next.js, Threejs and Tailwind CSS.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='antialiased'>
-      <head />
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/x-icon" />
+      </head>
       <body>
         {children}
         <Analytics />
